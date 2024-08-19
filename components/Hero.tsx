dc18b10048c124a234042d11c29b765a2e6047e3
João Samuel Diogo <joaosamueldiogo@gmail.com>
2024-08-19 15:34:28 +0100

    hero remake

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import { FlipWords } from './ui/flip-words'
-import MagicButton from './ui/magic-button';
 import { AuroraBackground } from './ui/aurora-background';
 import { FaLocationArrow } from 'react-icons/fa';
-import Link from 'next/link';
 import { oswald } from './ui/fonts';
 import { cn } from '@/lib/utils';
+import LinkButton from './ui/button-link';
 
 const Hero = () => {
-	const words = ["CANTAR", "Viver", "Saborear"];
+	const words = ["CANTAR", "VIVER", "SABOREAR"];
   return (
     <div className='relative w-full'>
       <AuroraBackground className='hidden md:dark:block absolute inset-0' />
 
-      <section className="relative flex flex-col md:flex-row justify-center md:justify-between gap-10 items-center h-[80vh] w-full p-12 select-none">
-        <div className={cn("text-6xl md:text-7xl font-[100] text-left w-full text-surface-dark-300 dark:text-surface-300", oswald.className)}>
+      <section className="relative flex flex-col justify-center gap-10 items-center h-[80vh] w-full p-12 select-none">
+        <div className={cn("text-6xl md:text-7xl font-[100] text-center w-full text-surface-dark-300 dark:text-surface-300", oswald.className)}>
           Bute lá <br />
-          <FlipWords words={words} className="font-extrabold text-7xl md:text-8xl text-primary-100" />
+          <FlipWords words={words} className="font-extrabold text-7xl md:text-8xl text-surface-dark-100 dark:text-surface-500 " />{/*text-primary-100*/}
         </div>
-        <Link href="/songs" className='w-full'>
-          <MagicButton text='Bute' icon={<FaLocationArrow />} />
-        </Link>
+        <LinkButton  href="/songs" text='Bute' icon={<FaLocationArrow />} textOnMobile className="flex justify-center items-center"/>
       </section>
     </div>
 
--- components/ui/button-link.tsx
+++ components/ui/button-link.tsx
@@ -1,15 +1,16 @@
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
 
-const ButtonLink = ({href, text, icon}:{href: string, text:string, icon?:React.ReactNode}) => {
+const LinkButton = ({href, text, icon, className, textOnMobile}:{href: string, text:string, icon?:React.ReactNode, className?:string, textOnMobile?:boolean}) => {
   return (
-    <Link href={href}>
+    <Link href={href} className={cn('w-full', className)}>
         <button className="flex items-center justify-center gap-2 w-full md:w-[20rem] shadow-[0_4px_14px_0_rgb(100,161,71,39%)] hover:shadow-[0_6px_20px_rgba(100,161,71,23%)] bg-primary-200 rounded-md px-8 py-2 text-surface-500 font-light transition duration-200 ease-linear z-50">
-            <span className="hidden md:block">{text}</span>
+            <span className={textOnMobile?"":"hidden md:block"}>{text}</span>
             {icon}
         </button>
     </Link>
   )
 }
 
-export default ButtonLink
+export default LinkButton
